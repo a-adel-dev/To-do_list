9cac5de2e22418c00259ddd1a6ff5119751188d7
Ahmad A. Fouad <a.adel.dev@gmail.com>
2023-06-20 00:15:40 +0300

    Add task view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   mode: "jit",
   content: ["./src/source.html"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      green: colors.green,
+      orange: colors.orange,
+      slate: colors.slate,
+      blue: colors.blue,
+      accent: "#0C788F",
+    },
     extend: {
       height: {
         screen: ["100vh", "100dvh"],
